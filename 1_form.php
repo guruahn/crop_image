355ce51262851aec877482fb2c19a28be583e951
guruahn <guruahn@gmail.com>
2015-02-27 18:36:25 +0900

    image upload form
    - layout style
    - add foundation framwork
    - off canvas style

--- 1_form.php
+++ 1_form.php
@@ -2,58 +2,78 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="ko" xml:lang="ko">
 <head>
-	<title>NUBY 3차 프로모션 </title>
+	<title>Image Crop </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<link rel="stylesheet" href="http://mnuby.greaten.co.kr/app/event/feature_event/nuby_promotion_20140616/promotion3.css">
-	<link rel="stylesheet" media="screen and (max-width:720px)" href="http://mnuby.greaten.co.kr/app/event/feature_event/nuby_promotion_20140616/promotion3_mobile.css" />
-	<style type="text/css">
-	label{}
-	label img{}
-	</style>
+
+    <link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/app.css">
+
+
 </head>
 <body>
-<form id="infomation" name="infomation" action="/app/event/feature_event/nuby_promotion_20140616/2_process.php" enctype="multipart/form-data" method="POST" />
-	<fieldset>
-	<div class="step1">
-		<p><img src="q1_title.jpg" alt="우리 아니는 공주님인가요, 왕자님인가요?" style="width:100%;"/></p>
-		<div class="step1_contents">
-		<p class="p_image" style="overflow:hidden"><label for="girl"><img src="q1_contents_left.png" alt="" /></label>
-			<label for="boy"><img src="q1_contents_right.png" alt="" /></label>
-		</p>
-		<p class="p_radio">
-			<span style="margin-left:26.15384615384615%;">
-			<input type="radio" name="sex" value="girl" id="girl">
-			<input type="radio" name="sex" value="boy" id="boy" style="margin-left:38.23668639053254%;">
-			</span>
-		</p>
-		</div>
-		<p style="clear:both"><a href="#" class="sex"><img src="next_btn.jpg" alt="" style="width:100%;" /></a></p>
-		<p><img src="step1_bar.jpg" alt="" style="width:100%;"/></p>
-		
-	</div>
-	<div class="step2" style="display:none;">
-		<p><img src="q2_title.jpg" alt="Q2. 우리 아이의 이름을 입력해주세요" style="width:100%;"/></p>
-		<div class="step2_contents">
-		<input type="text" name="name" id="name" maxlength="5" placeholder="이름을 입력해주세요.(최대5자)" >
-		</div>
-		<p style="clear:both"><a href="#" class="name"><img src="next_btn2.jpg" alt="" style="width:100%;"/></a></p>
-		<p><img src="step2_bar.jpg" alt="" style="width:100%;"/></p>
-	</div>
-	<div class="step3" style="display:none;">
-		<p><img src="q3_title.jpg" alt="Q3. 우리 아이의 사진을 업로드 해주세요" style="width:100%;"/></p>
-		<div class="step3_contents">
-		</div>
-		<p class="file_upload">
-			<a href="#">
-				<input type="file" name="file" id="file" class="file" data-width-limit="490" data-height-limit="620" capture><img src="upload_btn.jpg" alt="" style="width:100%;"/>
-			</a>
-		</p>
-		<p><img src="step3_bar.jpg" alt="" style="width:100%;"/></p>
-	</div>
-	</fieldset>
-</form>
+<div id="wrapper" class="small-12 medium-9 small-centered large-centered columns">
+    <div class="off-canvas-wrap" data-offcanvas>
+        <div class="inner-wrap">
+            <nav class="tab-bar">
+                <section class="left-small">
+                    <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+                </section>
+
+                <section class="middle tab-bar-section">
+                    <h1 class="title">Image Crop</h1>
+                </section>
+
+                <section class="right-small">
+                    <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+                </section>
+            </nav>
+
+            <aside class="left-off-canvas-menu">
+                <ul class="off-canvas-list">
+                    <li><label>Foundation</label></li>
+                    <li><a href="#">The Psychohistorians</a></li>
+                    <li><a href="#">...</a></li>
+                </ul>
+            </aside>
+
+            <section class="main-section">
+                <form id="infomation" name="infomation" action="2_process.php" enctype="multipart/form-data" method="POST" />
+                    <fieldset>
+
+                        <div class="step2">
+                            <p>이미지에 넣을 텍스트</p>
+                            <div class="step2_contents">
+                                <input type="text" name="text" id="text" maxlength="5" placeholder="이미지에 넣을 텍스트를 입력해주세요.(최대5자)" >
+                            </div>
+                            <p style="clear:both"><a href="#" class="text"></a></p>
+                        </div>
+                        <div class="step3">
+                            <p>사진업로드</p>
+                            <div class="step3_contents">
+                            </div>
+                            <p class="file_upload">
+                                <a href="#">
+                                    <input type="file" name="file" id="file" class="file" data-width-limit="490" data-height-limit="620" capture>
+                                </a>
+                            </p>
+                        </div>
+                    </fieldset>
+                </form>
+            </section>
+
+            <a class="exit-off-canvas"></a>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+<script src="js/vendor/modernizr.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/foundation.min.js"></script>
 <script type="text/javascript">
 
 /*======
@@ -63,64 +83,15 @@ With jQuery: $(function() { ...code here... })
 Or window.onload = function() { ...code here ...}
 Or document.addEventListener('DOMContentLoaded', function(){ ...code here... }, false)
 =======*/
+$(document).foundation();
 $(document).ready(function(){
-	$('.sex').on('click', function(e){
-		var checkbox = $("input[name='sex']:checked");
-		if( checkbox.length > 0 ) {
-		    $('.step1').hide();
-		    $('.step2').fadeIn();
-		    return false;
-		} else {
-		    alert('공주님인가요, 왕자님인가요?'); // error
-		    return false;
-		}
-	    
-    });
-	$('.name').on('click', function(e){
-		var obj = document.getElementsByName('name')[0];
-		var inputname = obj.value.length;
-		var chktext = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
-		var result1 = true;
-		var result2 = true;
-		if(chktext.test(obj.value)){
-			alert("특수문자는 입력하실 수 없습니다.");
-			obj.value = "";
-			obj.focus();
-			result1 = false;
-		}
-		if(inputname>5 || inputname ==0){
-			alert('이름을 입력해주세요.(입력가능한 이름의 길이는 1~5자 입니다.)');
-			obj.focus();
-		    result2 = false;
-		}
-		if(result1 && result2) {
-			$('.step2').hide();
-		    $('.step3').fadeIn();
-			return false;
-		}
-		
-	});
-	$('.submit').on('click', function(e){
-	});
 
 	//파일업로드 시 파일 사이즈와 타입 체크
 	$('.file').change(function(){
 		imageSizeCheck($(this));
 	});
-	//엔터키 폼 전송 막기
-	$("input:text").keydown(function(evt) {
-        if (evt.keyCode == 13)
-            return false;
-    });
+
 });
-
-
-	function parentIframeResize()
-	{
-		var height = getParam('height');
-		// This works as our parent's parent is on our domain..
-		parent.resizeIframe(height);
-	}
 
 	// Helper function, parse param from request string
 	function getParam( name )
@@ -193,16 +164,6 @@ $(document).ready(function(){
 
 	}//end function
 </script>
-<!--구글추적코드-->
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-51671118-1', 'greaten.co.kr');
-ga('send', 'pageview');
-
-</script>
 </body>
 </html>
